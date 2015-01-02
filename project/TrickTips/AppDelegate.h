f7ee9707f37dc3da7ec6a1e7e9ed1f6575172a27
JakubDziworski <kuba@Kubas-MacBook-Pro.local>
2015-01-02 17:16:47 +0100

    ui placeholderowe

---
+++ project/TrickTips/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrickTips
+//
+//  Created by Kuba on 02/01/15.
+//  Copyright (c) 2015 Kuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project/TrickTips/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TrickTips
+//
+//  Created by Kuba on 02/01/15.
+//  Copyright (c) 2015 Kuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
